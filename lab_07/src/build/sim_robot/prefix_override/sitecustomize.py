0d25ac766a97e2d6688179afe4dcb97afde6df10
yasanthaniroshan <niroshanyi@gmail.com>
2024-03-28 21:23:43 +0530

    Complete Lab 07 - Connected Webots with ros - Use given tutorials run sample programs

---
+++ lab_07/src/build/sim_robot/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/yasantha/tutorials/ros_tutorial/lab_07/src/install/sim_robot'
---
+++ lab_07/src/sim_robot/launch/robot_launch.py
@@ -0,0 +1,41 @@
+
+import os
+import launch
+from launch_ros.actions import Node
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from webots_ros2_driver.webots_launcher import WebotsLauncher
+from webots_ros2_driver.webots_controller import WebotsController
+
+
+def generate_launch_description():
+    package_dir = get_package_share_directory('sim_robot')
+    robot_description_path = os.path.join(package_dir, 'resource', 'my_robot.urdf')
+
+    webots = WebotsLauncher(
+        world=os.path.join(package_dir, 'worlds', 'my_world.wbt')
+    )
+
+    my_robot_driver = WebotsController(
+        robot_name='my_robot',
+        parameters=[
+            {'robot_description': robot_description_path},
+        ]
+    )
+
+    obstacle_avoider = Node(
+        package='sim_robot',
+        executable='obstacle_avoider',
+    )
+
+    return LaunchDescription([
+        webots,
+        my_robot_driver,
+        obstacle_avoider,
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=webots,
+                on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
+            )
+        )
+    ])
